27370054f462e32a1236449ffa9c097737e7c2b8
alymurtazamemon <alimurtaza-assistant@outlook.com>
2025-02-20 16:06:17 +0100

    feat(App.tsx): use useFetch hook to fetch data and show it on UI.

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,36 @@
+import useFetch from "./hooks/useFetch";
+
+interface User {
+  id: number;
+  name: string;
+  email: string;
+}
+
 function App() {
-  return <h1>Hello World</h1>;
+  const { data, error, loading } = useFetch<User[]>(
+    "https://jsonplaceholder.typicode.com/users"
+  );
+
+  if (loading) {
+    return <h1>Loading</h1>;
+  }
+
+  if (error) {
+    return <h1>{error}</h1>;
+  }
+
+  return (
+    <>
+      <h1>Data Fetching in React + TypeScript</h1>
+      <ul>
+        {data.map((user) => (
+          <li key={user.id}>
+            {user.id}: Name: {user.name}, Email: {user.email}
+          </li>
+        ))}
+      </ul>
+    </>
+  );
 }
 
 export default App;
